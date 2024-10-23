e496212d78f61aafce9dd951516216721902a844
enalto <enaltogondrige@gmail.com>
2024-10-23 15:17:36 -0300

    add web data model.

---
+++ src/main/java/com/enalto/springframework/web/model/BeerStyleEnum.java
@@ -0,0 +1,14 @@
+package com.enalto.springframework.web.model;
+
+public enum BeerStyleEnum {
+    LARGER,
+    PILSNER,
+    STOLT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+}
